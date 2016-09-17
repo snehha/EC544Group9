86c1c9546d997b17c5e8213f6632fe12bec69ce1
Sneha Pradhan <snehapradhan@wireless1x-168-122-0-138.bu.edu>
2016-09-17 15:44:31 -0400

    Updates to Arduino Code

--- Challenge_1/xbee_temperature/xbee_temperature.ino
+++ Challenge_1/xbee_temperature/xbee_temperature.ino
@@ -11,44 +11,32 @@ double floatVal = 1.2;
 char charVal[10];
 char access;
 char key;
+int random_number;
+
 
 int samples[NUMSAMPLES];
-bool join = false;
+bool join = true;
 
 void initial_join() {
-  XBee.write("{\"id\": 1, \"temp\": 75} \n");
-  //XBee.write("Join1\n");
-  //id "key_string"
-  access = XBee.read();
-  Serial.write(access);
+  //XBee.write("{\"id\": 1, \"temp\": 75}\n");
+  random_number = random(1,10000);
+  XBee.write("J");                  // Send a join message to XBee
+  XBee.write(random_number);
+  XBee.write("\n");
+  access = XBee.read();             // Receive an "i" to get the id number
+  Serial.write(access);             // Debugging
   if(access == 'i') {
-    while(XBee.available() > 0) {
-      key = XBee.read();
+    while(XBee.available() > 0) {   // if access is "i", 
+      key = XBee.read();            // then get the is number
       //Serial.write(key);
     }
-    Serial.write(key);
-    join = true;
+    Serial.write(key);              // Debugging
+    join = true;                    // Continue with the temperature readout
   }
 }
 
-void setup(void) {
-  // put your setup code here, to run once:
-  XBee.begin(9600);
-  Serial.begin(9600);
-}
-
-void loop(void) {
-  //Ask if join
-  while(!join) {
-    initial_join();
-    delay(5000);
-  }
-  if(XBee.available() > 0){
-    if((XBee.read()) == 'r'){
-      join = false;
-    }
-  }
-  
+void get_temp(){
+  //Temperature code begins
   uint8_t i;
   float average;
   
@@ -69,16 +57,43 @@ void loop(void) {
   average = SERIESRESISTOR / average;
 
   float f_temp = 0;
+  // Calculating the temperature
   float steinhart;
   steinhart = log (average / THERMISTORNOMINAL);     // ln (R/Ro)
   steinhart = steinhart/BCOEFFICIENT;               // ln(R/Ro)/B
   steinhart += (1.0 / (TEMPERATURENOMINAL + 273.15)); // + (1/To)
   steinhart = 1.0 / steinhart;                 // Invert
-  steinhart =  steinhart - 273.15;                         // convert to C
-  f_temp = (steinhart * 9.0)/ 5.0 + 32.0;
+  steinhart =  steinhart - 273.15;             // convert to C
+  f_temp = (steinhart * 9.0)/ 5.0 + 32.0;      //Convert to F
+  // Converting from float to a character
   dtostrf(f_temp, 4, 2, charVal);
- // XBee.write("{ \"id\": %d, \"temp\": }")
-  XBee.write(charVal);
-  XBee.write("\n");
+  Serial.write(charVal);
+  Serial.write("\n");
+//  XBee.write("{ \"id\": ");
+//  XBee.write(key);
+//  XBee.write("\"temp\": ");
+//  XBee.write(charVal);
+//  XBee.write("}");
+//  XBee.write("\n");
+  delay(1000);
+}
+void setup(void) {
+  XBee.begin(9600);
+  Serial.begin(9600);
+}
+
+void loop(void) {
+  // Ask to join initially
+  while(!join) {
+    initial_join();
+    delay(5000);
+  }
+  // If the coordinator wants to reset
+  if(XBee.available() > 0){
+    if((XBee.read()) == 'r'){
+      join = false;
+    }
+  }
+  get_temp();
   delay(1000);
 }
