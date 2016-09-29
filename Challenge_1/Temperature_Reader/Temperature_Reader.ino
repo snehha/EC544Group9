09aae108994daa0274bab7948a84606f00c1e919
Sneha Pradhan <snehapradhan@44-22-41-155-wireles1x.bu.edu>
2016-09-29 15:31:26 -0400

    Temperature reader for bytes

--- Challenge_1/Temperature_Reader/Temperature_Reader.ino
+++ Challenge_1/Temperature_Reader/Temperature_Reader.ino
@@ -9,7 +9,7 @@
 #define TEMPERATURENOMINAL 25 
 
 int samples[NUMSAMPLES];
-
+char temp[10] = "";
 void setup(void) {
   Serial.begin(9600);
 }
@@ -17,39 +17,44 @@ void setup(void) {
 void loop(void) {
   uint8_t i;
   float average;
- 
+  
   // take N samples in a row, with a slight delay
   for (i=0; i< NUMSAMPLES; i++) {
    samples[i] = analogRead(THERMISTORPIN);
    delay(10);
   }
- 
+  
   // average all the samples out
   average = 0;
   for (i=0; i< NUMSAMPLES; i++) {
      average += samples[i];
   }
   average /= NUMSAMPLES;
-
-  Serial.print("Average analog reading "); 
-  Serial.println(average);
-
+  
   average = 1023 / average - 1;
   average = SERIESRESISTOR / average;
-  Serial.print("Thermistor resistance "); 
-  Serial.println(average);
-
+  
   float f_temp = 0;
   float steinhart;
+  int find_period;
+  
   steinhart = log (average / THERMISTORNOMINAL);     // ln (R/Ro)
   steinhart = steinhart/BCOEFFICIENT;               // ln(R/Ro)/B
   steinhart += (1.0 / (TEMPERATURENOMINAL + 273.15)); // + (1/To)
   steinhart = 1.0 / steinhart;                 // Invert
-  steinhart =  steinhart - 273.15;              // convert to C
-  f_temp = (steinhart * 9.0)/ 5.0 + 32.0;       // Convert to F
-  Serial.print("Temperature "); 
-  Serial.print(f_temp);
-  Serial.println(" Degrees F");
+  steinhart =  steinhart - 273.15;                         // convert to C
+  f_temp = (steinhart * 9.0)/ 5.0 + 32.0;
+  String string_temperature = String(f_temp, 2);
+//  dtostrf(f_temp, 4, 2, temp);
+//  Serial.println(f_temp);
+//  String string_temperature = String(temp);
+  find_period = string_temperature.indexOf('.');
+  String whole_temp = string_temperature.substring(0,find_period);
+  String float_temp = string_temperature.substring(find_period+1);
+  Serial.print("The whole number:");
+  Serial.println(whole_temp);
+  Serial.print("The float number:");
+  Serial.println(float_temp);
   
   delay(1000);
 }
