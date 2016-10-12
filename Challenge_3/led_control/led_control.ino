c7c5f61fdf285548396de5fb363d85b4fa6662db
Sneha Pradhan <snehapradhan@wireless1x-168-122-15-136.bu.edu>
2016-10-12 19:19:33 -0400

    led latest

--- Challenge_3/led_control/led_control.ino
+++ Challenge_3/led_control/led_control.ino
@@ -1,8 +1,8 @@
 #include <SoftwareSerial.h>
 
-int redPins[3] = {13,10,7};
-int greenPins[3] = {12,9,6};
-int bluePins[3] = {11,8,5};
+int redPins[3] = {13, 10, 7};
+int greenPins[3] = {12, 9, 6};
+int bluePins[3] = {11, 8, 5};
 int numPins = 3;
 
 SoftwareSerial XBee(2, 3); // RX, TX
@@ -27,64 +27,63 @@ void setup() {
   start = false;
   XBee.begin(9600);
   Serial.begin(9600);
-  for( int i = 0; i < numPins; i++) {
-    pinMode(redPins[i],OUTPUT);
-    pinMode(greenPins[i],OUTPUT);
-    pinMode(bluePins[i],OUTPUT);
+  for ( int i = 0; i < numPins; i++) {
+    pinMode(redPins[i], OUTPUT);
+    pinMode(greenPins[i], OUTPUT);
+    pinMode(bluePins[i], OUTPUT);
   }
 }
 
 
 
-void init_status(){
-  char first_byte = 50; //which LED
-  char second_byte = 80;   // R
-  char third_byte = 90;    // G
-  char fourth_byte = 100;   // B
-  
+void init_status() {
+  char first_byte = 255; //which LED
+  char second_byte = 255;   // R
+  char third_byte = 255;    // G
+  char fourth_byte = 255;   // B
+
   Serial.println("DATA: ");
   sprintf(data,  "%c%c%c%c", first_byte, second_byte, third_byte, fourth_byte);
   Serial.println(data);
   XBee.write(data);
   //start = true;
-    delay(2000);
+  delay(2000);
   //Check if the server is sending data
   bool server_running = false;
-//  if(XBee.available() > 0){
-//      Serial.println("Starting to read the XBee.");
-//      uint32_t byteRead = XBee.read();
-//      Serial.write(byteRead);
-//   }
-  if(XBee.available() > 0){
+  //  if(XBee.available() > 0){
+  //      Serial.println("Starting to read the XBee.");
+  //      uint32_t byteRead = XBee.read();
+  //      Serial.write(byteRead);
+  //   }
+  if (XBee.available() > 0) {
     server_running = true;
-  }  
-  
-  while(server_running) {
-    if(XBee.available() > 0){
+  }
+
+  while (server_running) {
+    if (XBee.available() > 0) {
       //Serial.println("Server_Running is true. Starting to read the XBee.");
       uint32_t byteRead = XBee.read();
       Serial.println(byteRead);
-        command[count] = byteRead;
-        Serial.println(command[count]);
-        count++;
-      if(count == 4) {
+      command[count] = byteRead;
+      Serial.println(command[count]);
+      count++;
+      if (count == 4) {
         count = 0;
         int led_to_turn_on;
         Serial.print("Command[2]: ");
         Serial.println(command[2]);
-        
         set_color(command[0], command[1], command[2], command[3]);  // red
       }
-        start = true;
+      start = true;
     }
-  }    
+  }
 }
-void set_color(int id, int red, int green, int blue){
-  #ifdef COMMON_ANODE
-    red = 255 - red;
-    green = 255 - green;
-    blue = 255 - blue;
-  #endif
+void set_color(int id, int red, int green, int blue) {
+#ifdef COMMON_ANODE
+  red = 255 - red;
+  green = 255 - green;
+  blue = 255 - blue;
+#endif
   analogWrite(redPins[id], red);
   analogWrite(greenPins[id], green);
   analogWrite(bluePins[id], blue);
@@ -93,30 +92,12 @@ void loop() {
   // put your main code here, to run repeatedly:
   // ledID R G B
   //if(Serial.read() == '*'){
-    while(!start){ 
-      Serial.println("Parsing the command sent");
-      delay(2000);
-      init_status();
-    }
-//    int led_to_turn_on;
-//    //digitalWrite(13, 1);
-//    switch(led_id_int) {
-//      case 1: 
-//        led_to_turn_on = 8;
-//         break;
-//      case 2:
-//        led_to_turn_on = 9;
-//        break;
-//      case 3:
-//        led_to_turn_on = 10;
-//        break;
-//    }
-    //Serial.print("Turned on: ");
-    //Serial.println(led_to_turn_on);
-//    digitalWrite(led_to_turn_on, HIGH);
-    //digitalWrite(led_id_int, 1);
-    //digitalWrite(led_id_int, 1);
-  
-  
-    delay(1000);
+  while (!start) {
+    Serial.println("Parsing the command sent");
+    delay(2000);
+    init_status();
+  }
+
+
+  delay(1000);
 }
