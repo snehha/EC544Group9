770cbc645b792fa7f48e2a26142ebd63d0506a2c
Santiago Beltran <sbeltran@bu.edu>
2016-10-12 22:39:57 -0400

    Web compatibility with ardunio

--- Challenge_3/ledServer.js
+++ Challenge_3/ledServer.js
@@ -1,26 +1,30 @@
 var app     =     require("express")();
-var SerialPort = require("serialport");
+//var SerialPort = require("serialport");
 var http    =     require('http').Server(app);
 var io      =     require("socket.io")(http);
 
 app.get('/', function(req, res){
-  res.sendFile(__dirname + '/test.html');
+  res.sendFile(__dirname + '/webview/test.html');
 });
 
-var portName = process.argv[2],
-portConfig = {
-	baudRate: 9600,
-	parser: SerialPort.parsers.byteLength(4)
-};
-var sp = new SerialPort(portName, portConfig);
+app.get('/1k.js', function(req, res){
+  res.sendFile(__dirname + '/webview/1k.js');
+});
+
+// var portName = process.argv[2],
+// portConfig = {
+// 	baudRate: 9600,
+// 	parser: SerialPort.parsers.byteLength(4)
+// };
+// var sp = new SerialPort(portName, portConfig);
 var ledNum = 3;
-var ledMap = {}; // ledID : [on/off,R,G,B]
+var ledMap = {}; // ledID : [sR,G,B]
 //Create Buffer object to pass to pass to sp.write
 var buffer = Buffer.allocUnsafe(4);
 
 for( var i = 1; i <= ledNum; i++) {
-    	var led = [0,255,0,0]; // [on/off , R, G, B]
-    	ledMap[i] = led;
+    	var led = [255,0,0]; // [on/off , R, G, B]
+    	ledMap[['bulb-' + i]] = led;
 	}
 
 io.on('connection', function(socket){
@@ -43,7 +47,7 @@ io.on('connection', function(socket){
 	ledCommand = (ledCommand << 8) | B;
 	buffer.writeInt32BE(ledCommand,0);
 	console.log("Sending command ID,R,G,B: " + buffer.toString('hex'));
-	sp.write(buffer);
+	//sp.write(buffer);
   })
 
 });
@@ -52,17 +56,16 @@ http.listen(3000, function(){
   console.log('listening on *:3000');
 });
 
-sp.on("open", function () {
-  console.log('open');
-
-  // receives status of LEDs (on/off and color)
-  sp.on('data', function(data) {
-    console.log('data received: ' + data);
-    //io.emit("chat message", "An XBee says: " + data);
-	for(var key in data) {
-		ledMap[key] = data[key]; // [on/off , R, G, B]
-	}
-  });
-
-});
-
+// sp.on("open", function () {
+//   console.log('open');
+//
+//   // receives status of LEDs (on/off and color)
+//   sp.on('data', function(data) {
+//     console.log('data received: ' + data);
+//     //io.emit("chat message", "An XBee says: " + data);
+// 	for(var key in data) {
+// 		ledMap[key] = data[key]; // [on/off , R, G, B]
+// 	}
+//   });
+//
+// });
