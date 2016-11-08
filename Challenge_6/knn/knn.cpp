f697b1986132581d6ffb49b180be95c5aaec35d2
Wasim Khan <wasimk95@bu.edu>
2016-11-07 19:36:00 -0500

    servo

---
+++ Challenge_6/knn/knn.cpp
@@ -0,0 +1,16 @@
+#include "knn.h"
+
+using namespace std;
+#include <vector>
+
+int euclidean(vector<int> a, vector<int> b, int length){
+
+}
+
+int totalEuc(vector<int> instance, vector<vector<int> > test){
+
+}
+
+int main(){
+
+}
