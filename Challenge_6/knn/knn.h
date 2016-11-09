6cad4d37612479b7d03ff5a819a26ebc2d869409
Wasim Khan <wasimk95@bu.edu>
2016-11-08 22:06:29 -0500

    knn c++ base

--- Challenge_6/knn/knn.h
+++ Challenge_6/knn/knn.h
@@ -1,6 +1,9 @@
+#include <math.h>
 #include <vector>
+#include <map>
+
 using namespace std;
 
 int euclidean(vector<int> a, vector<int> b, int length);
-
 int totalEuc(vector<int> instance, vector<vector<int> > test);
+char* avgKNN(char** classLabels,vector<int> indeces);
