bd6f796be04a006e2c4f3f32383d5c192c7069db
Wasim Khan <wasimk95@bu.edu>
2016-11-10 19:01:49 -0500

    almost

--- Challenge_6/knnReading/parsedata.py
+++ Challenge_6/knnReading/parsedata.py
@@ -2,51 +2,46 @@ import csv
 import glob, os
 
 
+def parseData():
+    allAccessPoints = {}
 
-allAccessPoints = {}
-
-for file in glob.glob("*.txt"):
-    with open(file, 'rb') as csvfile:
-        cvsreader = csv.reader(csvfile, delimiter=',')
-        count = 1
-        for row in cvsreader:
-            if row:
-                regionName = file[:-4]
-                bssid = row[1]
-                if bssid is '03341138252243':
-                    pass
-                signal = row[2]
-                # BSSID not inside the dictionary
-                if (regionName + " " + str(count)) not in allAccessPoints:
-                    #allAccessPoints[regionName] = {}
-                    allAccessPoints[regionName + " " + str(count)] = [(bssid, signal)]
-                else:
+    for file in glob.glob("*.txt"):
+        with open(file, 'rb') as csvfile:
+            cvsreader = csv.reader(csvfile, delimiter=',')
+            count = 1
+            for row in cvsreader:
+                if row:
+                    regionName = file[:-4]
+                    bssid = row[1]
+                    if bssid is '03341138252243':
+                        pass
+                    signal = row[2]
+                    # BSSID not inside the dictionary
                     if (regionName + " " + str(count)) not in allAccessPoints:
+                        #allAccessPoints[regionName] = {}
                         allAccessPoints[regionName + " " + str(count)] = [(bssid, signal)]
                     else:
-                        allAccessPoints[regionName + " " + str(count)].append((bssid, signal))
-            else:
-                count += 1
+                        if (regionName + " " + str(count)) not in allAccessPoints:
+                            allAccessPoints[regionName + " " + str(count)] = [(bssid, signal)]
+                        else:
+                            allAccessPoints[regionName + " " + str(count)].append((bssid, signal))
+                else:
+                    count += 1
 
-regionAccessPoints = {}
+    regionAccessPoints = {}
 
-for key in allAccessPoints:
-    rssidlist = tuple(allAccessPoints[key])
-    regionName = key
-    regionAccessPoints[rssidlist] = regionName
+    for key in allAccessPoints:
+        rssidlist = tuple(allAccessPoints[key])
+        regionName = key
+        regionAccessPoints[rssidlist] = regionName
 
-# print allAccessPoints
-counter = 0
-for key in regionAccessPoints:
-    counter += 1
-    print regionAccessPoints[key] + ': ',
-    print str(key)
-
-
-
-
-
-
+    # print allAccessPoints
+    # counter = 0
+    # for key in regionAccessPoints:
+    #     counter += 1
+    #     print regionAccessPoints[key] + ': ',
+    #     print str(key)
+    return regionAccessPoints
 
 
 
---
+++ Challenge_6/knnReading/pyKnn.py
@@ -0,0 +1,79 @@
+import math
+import random
+import operator
+import csv
+import glob, os
+from collections import defaultdict
+from parsedata import parseData
+
+maxSet = parseData()
+trainingSet = dict(maxSet)
+pruneSet = dict(trainingSet)
+
+for x in maxSet:
+	if random.randrange(1,1000) > 100:
+		del pruneSet[x]
+	else:
+		del trainingSet[x]
+
+print len(trainingSet)
+print len(pruneSet)
+
+#Have to change so accepts tuple of tuples
+def euclideanDistance(instance1, instance2, length):
+	distance = 0
+	for x in instance1:
+		for y in instance2:
+			if(x[0] == y[0]):
+				distance += pow((int(x[1]) - int(y[1])), 2)
+				#print x[0]
+	return math.sqrt(distance)
+
+#Example test instance [(12314124,-75),(4124324,-25)]
+#Return value is the keys of the k nearest neighbors- iterate and grab labels
+def getNeighbors(testInstance, k):
+	distances = []
+	length = len(testInstance)
+	for sample in trainingSet:
+		dist = euclideanDistance(testInstance, sample, length)
+		distances.append((sample, dist))
+	distances.sort(key=operator.itemgetter(1))
+	neighbors = []
+	for x in range(k):
+		neighbors.append(distances[x][0])
+	return neighbors
+
+def getResponse(neighbors):
+	classVotes = {}
+	for x in neighbors:
+		response = trainingSet[x]
+		if response in classVotes:
+			classVotes[response] += 1
+		else:
+			classVotes[response] = 1
+	sortedVotes = sorted(classVotes.iteritems(), key=operator.itemgetter(1), reverse=True)
+	#print sortedVotes
+	return sortedVotes[0][0]
+
+
+def testAccuracy():
+	sizeTrainingSet = len(trainingSet)
+	count = 0;
+	for tester in pruneSet: 
+		most = getResponse(getNeighbors(tester,5))
+		print most
+		if(most == maxSet[tester]):
+	 		count+=1 
+	# ok = []
+	# for tester in pruneSet: 
+	# 	ok = getNeighbors(tester,5)
+	# 	break
+	#  	#if(getResponse(getNeighbors(tester,5)) == pruneSet[tester]):
+	#  		#count+=1 
+	# #for x in ok:
+	# 	#print x
+	# no = getResponse(ok)
+	# #print no
+	# #print count/sizeTrainingSet
+
+testAccuracy()
