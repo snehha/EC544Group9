919730978c1fc5b2cb256b801f9fd391a7cbe6a4
Wasim Khan <wasimk95@bu.edu>
2016-11-27 17:20:12 -0500

    bully working

--- Challenge_7/bullyAlgo/bullyAlgo.ino
+++ Challenge_7/bullyAlgo/bullyAlgo.ino
@@ -8,44 +8,54 @@
 #define button 8 
 
 #define ELECTION_TIMEOUT 3
-#define LEADER_CHECK_COUNT 15
-#define LEADER_CHECK_TIME 2000
+#define LEADER_CHECK_COUNT 3
+#define LEADER_CHECK_TIME 3000
 #define MAX_UID_SIZE 65535
 //Append UID to the election_start/end
 
 SoftwareSerial XBee(2, 3); // RX, TX
 
-bool leader_set;
-bool leader;
-bool leader_alive;
-uint16_t uid;
-uint16_t leader_uid;
-uint16_t previous_uid;
-byte *message_send = malloc(8);
-byte *message_receive = malloc(4);
+bool leader_set;  //Leader has been determined
+bool leader;  //I am the leader
+bool leader_alive;  //Received msg from leader after previous timeout check
+
+uint16_t uid; //My uid
+uint16_t leader_uid;  //Leader uid
+uint16_t previous_leader;
+
+int send_size = 9;
+int receive_size = 5;
+byte *message_send = malloc(send_size); //Send 8 bytes with first 4 bytes all 1s as preamble
+byte *message_receive = malloc(receive_size);  //Received 8 bytes but store 4 since the first 4 are preamble used for alligning
+
+//Counters for leader check, election, and debugging received messages
 int leader_check_count = 0;
 int election_count = 0;
 int xbee_avail_counter = 0;
+
+
+
+//Timer to send and receive
 Timer t;
 
 void setup() {
-  
   XBee.begin(9600);
   Serial.begin(9600);
 
   //Uid setup
   randomSeed(analogRead(0));
   uid = random(1, MAX_UID_SIZE);
+  Serial.print("Generated Random Number: ");
   Serial.println(uid);
 
   //Flags
   leader = true;
   leader_set = false;
   leader_alive = false;
-  leader_uid = 0;
-  previous_uid = 0;
-  clearArray(message_send,8);
-  clearArray(message_receive,4);
+  leader_uid = uid;
+  previous_leader = uid;
+  clearArray(message_send,send_size);
+  clearArray(message_receive,receive_size);
 
   int tickEvent = t.every(LEADER_CHECK_TIME,statusTimer);
 
@@ -54,6 +64,7 @@ void setup() {
   pinMode(blueLED, OUTPUT);
   pinMode(button, INPUT);
 
+  //Clear buffer on startup
   while(XBee.available())
     char dont_care = XBee.read();
   
@@ -61,22 +72,27 @@ void setup() {
 
 // lights LED states for leader, infected, not infected
 void lightLED() {
-  if(leader) {
-    digitalWrite(blueLED, HIGH);
+  if(!leader_set){
+    digitalWrite(blueLED, LOW);
     digitalWrite(greenLED, LOW);
     digitalWrite(redLED, LOW);
-    Serial.println("Leader Light");
   }
-  if(!leader) {
-    digitalWrite(blueLED, LOW);
-    digitalWrite(greenLED, HIGH);
-    digitalWrite(redLED, LOW);
-    Serial.println("Not Leader Light");
+  else{
+    if(leader) {
+      digitalWrite(blueLED, HIGH);
+      digitalWrite(greenLED, LOW);
+      digitalWrite(redLED, LOW);
+    }
+    if(!leader) {
+      digitalWrite(blueLED, LOW);
+      digitalWrite(greenLED, HIGH);
+      digitalWrite(redLED, LOW);
+    }
   }
 }
 
 void loop() {
-  //Leader_set will only be set true in runElection
+  //Leader_set will only be set true after runElection
   if(leader_set)
     listenOthers();
     
@@ -84,39 +100,33 @@ void loop() {
   t.update();
 }
 
-void statusTimer(){
-  //Check for leader while election is not going on
-  /***
-   *  After every LEADER_CHECK_COUNT timer runs, 
-   *  IF the leaderAlive is still false, then the leader is dead.
-   *  
-   *  ELSE then the leader is assumed to still be alive.
-   *  
-   *  The leaderAlive is set by the listenOthers() function, which runs
-   *  on the loop, it will read all messages but everytime it seems the
-   *  correct leader, it will flip/keep the leaderAlive bool bit true.
-   ***/
-  if(leader_set){
-    lightLED();
-    //Run if this Arduino is the leader
-    if(leader){
-      //Sent status
-      Serial.println("I'm the leader");
-      writeXBee(message_send,uid,1,1);
-    }
-    //Run if this is a client node Arduino
-    else{
-      //If the leader is still false, count up to the LEADER_CHECK_COUNT
-      if(!leader_alive){
-        //if the LEADER_CHECK_COUNT == leader timer check run, leader dead, set the leader_set to false
-        if(++leader_check_count == LEADER_CHECK_COUNT)
-          leader_set = false;
-      }
-      leader_alive = false;
-    }
-  }
+void clearArray(byte* message, int count){
+  for(int i = 0; i < count; i++)
+    message[i] = 0;
+}
+
+void printStatus(){
+  Serial.println("-------------------------Entering Print Status----------------------");
+  Serial.print("My Uid: ");
+  Serial.println(uid);
+  Serial.print("Leader uid: ");
+  Serial.println(leader_uid);
+  Serial.print("Leader Set: ");
+  if(leader_set)
+    Serial.println("True");
   else
-    runElection();
+    Serial.println("False");
+  Serial.print("I am Leader: ");
+  if(leader)
+    Serial.println("True");
+  else
+    Serial.println("False");
+  Serial.print("Leader Alive: ");
+  if(leader_alive)
+    Serial.println("True");
+  else
+    Serial.println("False");
+  Serial.println("-------------------------Leaving Print Status-----------------------");
 }
 
 void printMessage(byte* message,int count){
@@ -131,7 +141,7 @@ void printMessage(byte* message,int count){
     Serial.println((uint16_t)message[7]);
   }
   else{
-    Serial.print("\tUID: ");
+    Serial.print("UID: ");
     Serial.print((((uint16_t)(message[0])) << 8) | (uint16_t)(message[1]));
     Serial.print("\tCommand: ");
     Serial.print((uint16_t)message[2]);
@@ -152,24 +162,18 @@ void preamMes(byte *message, uint16_t uid, byte infectionStatus, byte leaderStat
   message[7] = leaderStatus;
 }
 
-void clearArray(byte* message, int count){
-  for(int i = 0; i < count; i++)
-    message[i] = 0;
-}
-
-
 void writeXBee(byte* message,uint16_t uid, byte infectionStatus, byte leaderStatus){
+  clearArray(message,send_size);
   preamMes(message, uid, infectionStatus, leaderStatus);
   XBee.write((char*)message);
   delay(10);
-  Serial.print("+++++++ Sending Data Log +++++++            ");
+  Serial.print("Data Send----------");
   printMessage(message,8);
 }
 
 
 void readXBee(){
-  clearArray(message_receive,4);
-  printMessage(message_receive,4);
+  clearArray(message_receive,receive_size);
 
   //Count four bytes and set xbeeAvailable to true
   int counter = 0;
@@ -181,11 +185,8 @@ void readXBee(){
 
   //ASSUMING packet is recieved with 8 bytes together.
   while(XBee.available()){
-    Serial.print("Counter XBEE:");
-    Serial.println(xbee_avail_counter++); 
     char readByte = XBee.read();
-    //Serial.print(int(readByte));
-    //Serial.println(" ");
+    Serial.println(int(readByte));
     if (((byte)readByte == 255) && !data_clean){
       count_ones++;
       if(count_ones == 4){
@@ -196,10 +197,10 @@ void readXBee(){
         continue;
     }
     else{
-      if(counter < 3){
+      if(counter < 4){
         message_receive[counter++] = readByte;
       }
-      else{
+      if(counter==4){
         xbeeAvailable = true;
         data_clean = false;
         break;
@@ -207,26 +208,23 @@ void readXBee(){
     }
   }
   if (xbeeAvailable) {
-    Serial.print("****** Received Data Log ********                 ");
+    Serial.print("Data Receive-------------");
     printMessage(message_receive,4);
   }
   else{
-    clearArray(message_receive,4);
+    clearArray(message_receive,receive_size);
   }
 }
 
 
 /*************During Election**************/
 void runElection(){
-  
-    Serial.println("Running Election");
-    if(leader_uid < uid){
-      //Serial.print("I'm running for prez with: ");
-      //Serial.println(uid);
-      Serial.print("Leader uid is: ");
-      Serial.println(leader_uid);
+    Serial.println("*****************Running Election******************");
+    if(leader_uid == uid){
+      Serial.print("I'm running for prez with: ");
+      Serial.println(uid);
       writeXBee(message_send,uid,1,1);
-      previous_uid = uid;
+      previous_leader = uid;
     }
 
     //****** PROCESS THE TEXT **********//
@@ -242,72 +240,64 @@ void readElection(){
     byte received_leader = message_receive[2];
     byte received_cmd = message_receive[3];
 
-    Serial.print("Uint16_t uid: ");
-    Serial.println(received_uid);
-
+    //Read buffer is empty
+    //Check for redundant leader messages
     if(!received_uid){
-      if(previous_uid == uid){
-        Serial.print("No read and alone -- UID: ");
-        Serial.println(previous_uid);
+      if(previous_leader == leader_uid){
+        //Once it goes in here, it will not run election and just listen untill it gets a new non leader message with leader byte set
         if(++election_count == ELECTION_TIMEOUT){
+          Serial.println("ELECTION_________________________________________________OVER");
           leader_set = true;
-          leader = true;
-          break;
+          lightLED();
+          election_count = 0;
         }
       }
-      previous_uid = uid;
+      leader_uid = previous_leader;
       break;
     }
-    //****** IF receivedID is ZERO don't do this below **********//
-    
-    if(received_uid < uid){
-      leader_uid = uid;
-      Serial.print("Im still in it: ");
-      Serial.println(uid);
-      //Change to four bytes
-      election_count++;
-      writeXBee(message_send,uid,1,1);
+
+    printMessage(message_receive,4);
+
+    //Still in it so listen
+    if(received_uid < uid && leader){
+      previous_leader = uid;
+      Serial.println("Received lower uid.Still Leader");
+      //election_count++;
+      //writeXBee(message_send,uid,1,1);
     }
-    if(received_uid > uid){
-      leader = false;
-      leader_uid = received_uid;
-      leader_set = true;
-      Serial.println("Lost");
-    }
+    //If someone bigger, then set leader to false
     if(received_uid > leader_uid){
+      Serial.print("Received Highest Leader UID: ");
+      Serial.println(received_uid);
+      leader = false;
+      previous_leader = received_uid;
+      //leader_set = true;
+    }
+    /*if(received_uid > leader_uid){
       leader_uid = received_uid;
       election_count++;
       leader_set = true;
       Serial.print("Received Highest uid: ");
       Serial.println(leader_uid);
     }
-    if(previous_uid == received_uid){
+    if(previous_leader == received_uid){
       Serial.print("Previous UID: ");
-      Serial.println(previous_uid);
+      Serial.println(previous_leader);
       if(++election_count == ELECTION_TIMEOUT){
         leader_set = true;
         leader = true;
         Serial.println("Leader Decided");
         break;
       }
-    }
-    
-    previous_uid = received_uid;
+    }*/
   }
-  
 }
 /****************************************/
 
 /**************After Election***************/
 //Listen for leader is alive - Leader will send alive message periodically
-
-
-int listenOthers(){
-
+void listenOthers(){
   while(1){
-    if(leader){
-      writeXBee(message_send,uid,1,1);
-    }
     //4 bytes received with 2 bytes ID, 1 byte leader, 1 byte cmd
     readXBee();
     uint16_t received_uid = ((((uint16_t)message_receive[0]) << 8) | (uint16_t)message_receive[1]);
@@ -315,31 +305,74 @@ int listenOthers(){
     byte received_cmd = message_receive[3];
   
     if(!received_uid){
-      Serial.println("xxxxxxxxxxxxxxxxxxxxx It's lonely in here xxxxxxxxxxxxxxxxxxxxxxxxxxxx");
       break;
     }    
-    //Received Leader data
+    //Received Leader data so execute command
     if(received_uid == leader_uid){
-      Serial.println("----------------------------------I HEARD THE LEARDEAR----------------------------------");
-      //Make sure it is leader for debugging by getting ID
+      Serial.println("----------------------------------I HEARD THE LEADER----------------------------------");
       if(received_leader == 1){
         leader_check_count = 0;
         leader_alive = true;
         //Command from leader
         if(received_cmd==1)
-          //return 1;
           //RUN CODE FOR CLEAR
           continue;
       }
      }
-    //Received Node data  
-    else if(received_leader==0)
-      //Infect
-      if(received_cmd==2)
-        continue;
-        //return 2;
-        //RUN CODE FOR INFECTION
+    //Someone else thinks they are leader
+    else if(received_leader==1){
+      if(leader){
+        if(received_uid > uid){
+          leader = false;
+          leader_set = false;
+          lightLED();
+        }
+      }
+    }
+    //Node wants to infect
+    else if(received_cmd==2)
+      //RUN CODE FOR INFECTION
+      continue;
   }
-  return 0;
 }
 /******************************************/
+
+void statusTimer(){
+  lightLED();
+  //Check for leader while election is not going on
+  /***
+   *  After every LEADER_CHECK_COUNT timer runs, 
+   *  IF the leaderAlive is still false, then the leader is dead.
+   *  
+   *  ELSE then the leader is assumed to still be alive.
+   *  
+   *  The leaderAlive is set by the listenOthers() function, which runs
+   *  on the loop, it will read all messages but everytime it seems the
+   *  correct leader, it will flip/keep the leaderAlive bool bit true.
+   ***/
+  Serial.println("++++++++++++++++++++++++Before Leader Check+++++++++++++++++++++++++");
+  printStatus();
+  //Will be false whenever it receives uid that is not leader with leader byte set, which is initially sent by all
+  if(leader_set){
+    //Run if this Arduino is the leader
+    if(leader){
+      //Sent status
+      Serial.println("I'm the leader");
+      writeXBee(message_send,uid,1,1);
+    }
+    //Run if this is a client node Arduino
+    else{
+      //If the leader is still false, count up to the LEADER_CHECK_COUNT
+      if(!leader_alive){
+        //if the LEADER_CHECK_COUNT == leader timer check run, leader dead, set the leader_set to false
+        if(++leader_check_count == LEADER_CHECK_COUNT)
+          leader_set = false;
+      }
+      leader_alive = false;
+    }
+  }
+  else
+    runElection();
+  printStatus();
+  Serial.println("++++++++++++++++++++++++After Leader Check+++++++++++++++++++++++++");
+}
