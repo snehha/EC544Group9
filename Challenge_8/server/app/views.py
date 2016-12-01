b3880218dfd3759588b1baf8e5eeaa2deb1bf75c
christineklow <clow@bu.edu>
2016-12-01 15:56:39 -0500

    website controls

---
+++ Challenge_8/server/app/views.py
@@ -0,0 +1,89 @@
+# from app import app
+from flask import Flask, send_file, request, Response, render_template
+from flask_socketio import SocketIO, send, emit
+#from pyKnn import *
+
+app = Flask(__name__)
+
+
+@app.route('/')
+@app.route('/index')
+def index():
+    return render_template('location.html')
+
+
+@app.route('/static/fourthfloor.png')
+def image():
+    return send_file('static//img//fourthfloor.png', mimetype='image/png')
+
+@app.route('/static/uparrow.png')
+def upArrowImage():
+    return send_file('static//img//uparrow.png', mimetype='image/png')
+
+@app.route('/static/downarrow.png')
+def downArrowImage():
+    return send_file('static//img//downarrow.png', mimetype='image/png')
+
+@app.route('/static/leftarrow.png')
+def leftArrowImage():
+    return send_file('static//img//leftarrow.png', mimetype='image/png')
+
+@app.route('/static/rightarrow.png')
+def rightArrowImage():
+    return send_file('static//img//rightarrow.png', mimetype='image/png')
+
+@app.route('/static/gobutton.png')
+def goButtonImage():
+    return send_file('static//img//gobutton.png', mimetype='image/png')
+
+@app.route('/static/stopbutton.png')
+def stopButtonImage():
+    return send_file('static//img//stopbutton.png', mimetype='image/png')
+
+
+socketio = SocketIO(app)
+
+
+def send_location(location):
+    socketio.emit('location_event', location)
+    index()
+
+
+region = 27
+bottomRightRegion = 15;
+limit = 37
+
+
+def testCoordinates():
+    global region
+    region = region + 1
+    if region is limit:
+        region = 53
+    if region is 55:
+        region = bottomRightRegion
+
+
+@socketio.on('refresh')
+def sendMessage():
+    global region
+    # print ('got refresh')
+    #emit('location_event', getPrediction())
+    emit('location_event', region);
+
+@socketio.on('moveCar')
+def moveCar(command):
+    print (command)
+
+@socketio.on('startCar')
+def moveCar(command):
+    print ("start car: ")
+    print(command)
+
+
+# testCoordinates()
+
+if __name__ == '__main__':
+    socketio.run(app)
+    app.run(host='0.0.0.0')
+
+#app.run(host='0.0.0.0', debug=True)
