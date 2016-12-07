49c8fb469fcca370ed870a396561c70f09765f81
Sneha Pradhan <snehha@bu.edu>
2016-12-07 01:23:30 -0500

    photon stuff

---
+++ Challenge_8/testing/test.ino
@@ -0,0 +1,41 @@
+#include "application.h"
+int led = D7;
+void setup()
+{
+    pinMode(led, OUTPUT);
+    digitalWrite(led, LOW);
+    Particle.function("SeverSendTest", serverSendTest);
+}
+
+int serverSendTest(String command)
+{
+    if(command == "up")
+    {
+        Serial.println("up");
+        return 1;
+    }
+    else if(command == "down")
+    {
+        Serial.println("down");
+        return 2;
+    }
+    else if(command == "left")
+    {
+        Serial.println("left");
+        return 3;
+    }
+    else if(command == "right")
+    {
+        Serial.println("right");
+        return 4;
+    }
+    else
+    {
+      return -1;
+    }
+}
+
+void loop()
+{
+    digitalWrite(led, HIGH);
+}
