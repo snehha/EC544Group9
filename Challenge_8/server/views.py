b9adfbf2ed77a53d7a69ad8836c453ddc80e9cd7
christineklow <clow@bu.edu>
2016-12-08 17:56:23 -0500

    send turns from python

--- Challenge_8/server/views.py
+++ Challenge_8/server/views.py
@@ -8,9 +8,35 @@ import threading
 from pyKnn import *
 from reqRSS import *
 
+import serial
+
+# Compass
+import time
+from Adafruit_HMC6352 import HMC6352
+
 currentLoc = ''
 knnRefresh = 2.0
+# clockwise = true
+# corners = [15, 24, 27, 36]
+# elevatorRegions = [53, 54]
+# previousLoc = 0;
 
+# Initialise the HMC6352
+hmc = HMC6352()
+hmc.userCalibration()
+
+photonSerial = serial.Serial(
+    port='/dev/ttyUSB1',
+    baudrate=9600,
+    parity=serial.PARITY_ODD,
+    stopbits=serial.STOPBITS_TWO,
+    bytesize=serial.EIGHTBITS
+)
+
+if photonSerial.isOpen():
+    photonSerial.close()
+photonSerial.open()
+photonSerial.isOpen()
 
 server = Flask(__name__)
 socketio = SocketIO(server)
@@ -52,6 +78,7 @@ def stopButtonImage():
 @server.route('/ssid', methods=['GET','POST'])
 def getSSIDs():
     global currentLoc
+    global previousLoc
     #Logic to get ssid from photon via webhook
     dataReceivedFromUpdate = request.data
     dataReceivedFromUpdate = tuple([tuple(group.split(',')) for group in ast.literal_eval(dataReceivedFromUpdate)])
@@ -60,21 +87,99 @@ def getSSIDs():
 
     # print("*****************: " + dataReceivedFromUpdate)
     if dataReceivedFromUpdate:
-        # PHTON --> LOCAL ClOUD -> WEBHOOK -> THIS FUNCTION
+        # PHOTON --> LOCAL ClOUD -> WEBHOOK -> THIS FUNCTION
         print("Got SSIDs from Photon")
         if (dataReceivedFromUpdate):
             newLoc = getPrediction(dataReceivedFromUpdate)
             if (currentLoc is newLoc):
                 return 'acknowledge but same'
             else:
+                if (currentLoc != '') :         # set previous location if there is one
+                    previousLoc = int(currentLoc)
                 currentLoc = newLoc
-                sendPrediction(currentLoc)
+                predictTurn()
+                #sendPrediction(currentLoc)
                 return 'status 200'
         else:
             return 'error 500'
     else:
         return 'error 500'
     return 'error 500'
+
+def getCompassReading():
+    heading = hmc.readData()
+    print "Compass Heading: %.1f" % heading
+    return heading
+
+north = 45
+east = 135
+south = 225
+west = 315
+compassThreshold = 10
+
+previousHeading = getCompassReading()
+
+def predictTurn():
+    current = int(currentLoc)
+
+    # get compass reading to determine if turn is needed in starting region
+    heading = getCompassReading
+    photonSerial.write("Comp:" + str(heading))
+
+    if ( current == 16 ) : 
+        if (heading >= east && heading <= (east + compassThreshold)) :
+            photonSerial.write("right")
+            return
+    if ( current == 23 ) : 
+        if (heading >= west && heading <= (west + compassThreshold)) :
+            photonSerial.write("left")
+            return
+    elif ( current == 25 ) :
+        if (heading >= north && heading <= (north + compassThreshold)) :
+            photonSerial.write("right")
+            return
+    elif ( current == 26 ) :
+        if (heading >= south && heading <= (south + compassThreshold)) :
+            photonSerial.write("left")
+            return
+    if ( current == 28 ) : 
+        if (heading >= west && heading <= (west + compassThreshold)) :
+            photonSerial.write("right")
+            return
+    elif ( current == 35 ) :
+        if (heading >= east && heading <= (east + compassThreshold)) :
+            photonSerial.write("left")
+            return
+    elif ( current == 53 ) :
+        if (heading >= south && heading <= (south + compassThreshold)) :
+            photonSerial.write("right")
+            return
+    elif ( current == 54 ) :
+        if (heading >= north && heading <= (north + compassThreshold)) :
+            photonSerial.write("left")
+            return
+
+    # global previousLoc 
+
+    # if (previousLoc == 0) :                 # no previous location yet
+    #     previousLoc = current
+
+    # if (current in elevatorRegions):        # turn if elevator region
+    #     if(current < previousLoc) :  
+    #         photonSerial.write("right")
+    #         return
+    #     else:
+    #         photonSerial.write("left")
+    #         return
+
+    # if (current < previousLoc):             # (clockwise) regions decrease
+    #     if ( (current - 1) in corners):     # if region is before a corner
+    #         photonSerial.write("right")     
+    #         return
+    # elif (current > previousLoc):           # (counterclockwise) regions increase
+    #     if ( (current + 1) in corners):     # if region is before a corner
+    #         photonSerial.write("left")      
+    #         return
 
 def send_location(location):
     socketio.emit('location_event', location)
